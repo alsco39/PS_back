870d169f1bebeb91171f5bd0e53ebd6b76cebee1
alsco39 <minchaeg85@gmail.com>
2024-03-22 22:44:30 +0900

    setting :: 프로젝트 생성

---
+++ src/test/kotlin/com/example/ps_back/PsBackApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.ps_back
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PsBackApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
