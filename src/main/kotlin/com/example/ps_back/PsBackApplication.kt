c021200a3f3f61ab9927f300b06eb4a283a65329
alsco39 <minchaeg85@gmail.com>
2024-05-13 23:10:52 +0900

    update :: @EnableConfigurationProperties 추가

--- src/main/kotlin/com/example/ps_back/PsBackApplication.kt
+++ src/main/kotlin/com/example/ps_back/PsBackApplication.kt
@@ -1,9 +1,12 @@
 package com.example.ps_back
 
+import com.example.ps_back.global.security.jwt.JwtProperties
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
+@EnableConfigurationProperties(JwtProperties::class)
 @EnableJpaAuditing
 @SpringBootApplication
 class PsBackApplication
