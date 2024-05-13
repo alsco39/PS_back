d245c85c3de711403f4173879ad9ef3791418e49
alsco39 <minchaeg85@gmail.com>
2024-05-13 23:10:23 +0900

    add :: JwtProperties

---
+++ src/main/kotlin/com/example/ps_back/global/security/jwt/JwtProperties.kt
@@ -0,0 +1,12 @@
+package com.example.ps_back.global.security.jwt
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+
+@ConfigurationProperties(prefix = "jwt")
+data class JwtProperties(
+    var secretKey: String,
+    val accessExp: Long,
+    val refreshExp: Long,
+    val header: String,
+    val prefix: String,
+)
