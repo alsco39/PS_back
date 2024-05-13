4ab97e30049a9e674bcc59ef07d58d9dbb90599e
alsco39 <minchaeg85@gmail.com>
2024-05-13 23:08:56 +0900

    add :: GlobalExceptionHandler

---
+++ src/main/kotlin/com/example/ps_back/global/error/GlobalExceptionHandler.kt
@@ -0,0 +1,27 @@
+package com.example.ps_back.global.error
+
+import com.example.ps_back.global.error.exception.PsException
+import org.springframework.http.HttpStatus
+import org.springframework.http.HttpStatus.BAD_REQUEST
+import org.springframework.http.ResponseEntity
+import org.springframework.validation.BindException
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+@RestControllerAdvice
+class GlobalExceptionHandler {
+
+    @ExceptionHandler(PsException::class)
+    fun customExceptionHandling(e: PsException): ResponseEntity<ErrorResponse> {
+        return ResponseEntity(ErrorResponse.of(e), HttpStatus.valueOf(e.status))
+    }
+
+    @ExceptionHandler(BindException::class)
+    fun handleBindException(e: BindException): ResponseEntity<*>? {
+        val errorMap: MutableMap<String, String?> = HashMap()
+        for (error in e.fieldErrors) {
+            errorMap[error.field] = error.defaultMessage
+        }
+        return ResponseEntity<Map<String, String?>>(errorMap, BAD_REQUEST)
+    }
+}
