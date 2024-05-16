982e91e37a2e3b90f4a4bc971e16143811594f25
alsco39 <minchaeg85@gmail.com>
2024-05-16 10:44:08 +0900

    update :: refresh 제거

--- src/main/kotlin/com/example/ps_back/global/security/jwt/JwtProperties.kt
+++ src/main/kotlin/com/example/ps_back/global/security/jwt/JwtProperties.kt
@@ -6,7 +6,6 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 data class JwtProperties(
     var secretKey: String,
     val accessExp: Long,
-    val refreshExp: Long,
     val header: String,
     val prefix: String,
 )
