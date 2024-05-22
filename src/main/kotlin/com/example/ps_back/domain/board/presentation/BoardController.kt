928fd150afd03ccccfb8b75770b065a7980b4989
min56 <minchaeg85@gmail.com>
2024-05-22 11:15:07 +0900

    add :: Board 수정 API

--- src/main/kotlin/com/example/ps_back/domain/board/presentation/BoardController.kt
+++ src/main/kotlin/com/example/ps_back/domain/board/presentation/BoardController.kt
@@ -1,9 +1,12 @@
 package com.example.ps_back.domain.board.presentation
 
 import com.example.ps_back.domain.board.presentation.dto.request.CreateBoardRequest
+import com.example.ps_back.domain.board.presentation.dto.request.UpdateBoardRequest
 import com.example.ps_back.domain.board.service.BoardService
 import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.PatchMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -22,4 +25,9 @@ class BoardController(
         boardService.createBoard(request)
     }
 
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PatchMapping("/update/{board-id}")
+    public fun updateBoard(@PathVariable("board-id") boardId: Long, @RequestBody @Valid request: UpdateBoardRequest) {
+        boardService.updateBoard(request, boardId)
+    }
 }
--- src/main/kotlin/com/example/ps_back/domain/board/service/BoardService.kt
+++ src/main/kotlin/com/example/ps_back/domain/board/service/BoardService.kt
@@ -3,7 +3,10 @@ package com.example.ps_back.domain.board.service
 import com.example.ps_back.domain.board.entity.Board
 import com.example.ps_back.domain.board.entity.enums.BoardType
 import com.example.ps_back.domain.board.entity.repository.BoardRepository
+import com.example.ps_back.domain.board.exception.BoardNotUpdateException
+import com.example.ps_back.domain.board.facade.BoardFacade
 import com.example.ps_back.domain.board.presentation.dto.request.CreateBoardRequest
+import com.example.ps_back.domain.board.presentation.dto.request.UpdateBoardRequest
 import com.example.ps_back.domain.user.facade.UserFacade
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -11,6 +14,7 @@ import org.springframework.transaction.annotation.Transactional
 @Service
 class BoardService(
     private val userFacade: UserFacade,
+    private val boardFacade: BoardFacade,
     private val boardRepository: BoardRepository,
 ) {
 
@@ -32,4 +36,23 @@ class BoardService(
         )
     }
 
+    @Transactional
+    public fun updateBoard(request: UpdateBoardRequest, boardId: Long) {
+        val board = boardFacade.getByBoardId(boardId)
+        if(!checkBoardWriter(board.id)) throw BoardNotUpdateException
+
+        board.updateBoard(
+            request.title,
+            request.startDate,
+            request.endDate,
+            request.note,
+            request.money,
+            request.area,
+        )
+    }
+
+    private fun checkBoardWriter(boardId: Long): Boolean {
+        val userId = userFacade.getCurrentUser().id
+        return userId.equals(boardId)
+    }
 }
