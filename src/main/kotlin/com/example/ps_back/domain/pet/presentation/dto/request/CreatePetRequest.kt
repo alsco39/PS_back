7f530ca40076bd8d8f759be5efb2e67ca5ae001c
min56 <minchaeg85@gmail.com>
2024-06-19 22:50:25 +0900

    update :: CreatePetRequest

--- src/main/kotlin/com/example/ps_back/domain/pet/presentation/dto/request/CreatePetRequest.kt
+++ src/main/kotlin/com/example/ps_back/domain/pet/presentation/dto/request/CreatePetRequest.kt
@@ -1,12 +1,11 @@
 package com.example.ps_back.domain.pet.presentation.dto.request
 
 import com.example.ps_back.domain.pet.entity.enums.Gender
-import com.example.ps_back.domain.pet.entity.enums.Tendency
 
 data class CreatePetRequest(
     val name: String,
     val gender: Gender,
     val age: Int,
-    val tendency: Tendency,
+    val tendency: String,
     val profile: String,
 )
